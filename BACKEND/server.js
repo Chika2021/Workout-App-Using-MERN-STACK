88d0e1233e2f9adf1fb7513c78e37baf39e7d9d8
Chika2021 <amaechichika9@gmail.com>
2024-04-07 19:40:35 +0000

    Implementing Mongoose Database

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,6 +3,8 @@ const workOutRoute = require('./routes/workout')
 
 const express = require('express')
 
+const mongoose = require('mongoose');
+
 const app = express()
 
 app.use(express.json())
@@ -14,9 +16,12 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workOutRoute);
 
+mongoose.connect(process.env.MONGO_URI)
+    .then( () => {
+        app.listen(process.env.PORT, () => {
+            console.log('Listening on Port', process.env.PORT)
+        })
+    } )
+    .catch((err => {console.log(err)}))
 
 
-
-app.listen(process.env.PORT, () => {
-    console.log('Listening on Port', process.env.PORT)
-})
