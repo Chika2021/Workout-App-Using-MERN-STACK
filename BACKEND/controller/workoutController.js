c1d1dfea2fd414c651f8e5766cfbb9e059a88fc4
Chika2021 <amaechichika9@gmail.com>
2024-04-08 23:57:47 +0000

    Creating API EndPoints

--- BACKEND/controller/workoutController.js
+++ BACKEND/controller/workoutController.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Workout = require('../models/workout')
 
-//CREATING WORKOUTS
+
 exports.createWorkout = async (req, res) => {
 
     const { title, load, reps } = req.body
@@ -15,14 +15,13 @@ exports.createWorkout = async (req, res) => {
 
 }
 
-//GET ALL WORKOUTS
+
 exports.getWorkouts = async (req, res) => {
     const workouts = await Workout.find({}).sort({ createdAt: -1 })
     res.status(200).json(workouts)
 }
 
 
-//GET WORKOUT BY ID
 exports.getWorkoutsById = async (req, res) => {
     const { id } = req.query
     const workout = await Workout.findById(id)
@@ -33,7 +32,7 @@ exports.getWorkoutsById = async (req, res) => {
     res.status(200).json(workout)
 }
 
-//UPDATE WORKOUT
+
 exports.updateWorkout = async (req, res) => {
     const { id, title, load, reps } = req.query
 
@@ -45,7 +44,7 @@ exports.updateWorkout = async (req, res) => {
 
 }
 
-//DELETE WORKOUT
+
 exports.deleteWorkout = async (req, res) => {
     const {id} = req.query
     const workout = await Workout.findByIdAndDelete(id)
