3d9de5ffe99c4cc87928ddbb3e79cd32bce747c3
Chika2021 <amaechichika9@gmail.com>
2024-04-09 01:45:02 +0000

    Adding NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
     <header>
+        <div className='container'>
+            <Link to = '/'>
+                <h1>Welcome Buddy</h1>
+            </Link>
+
+        </div>
       
     </header>
   )
